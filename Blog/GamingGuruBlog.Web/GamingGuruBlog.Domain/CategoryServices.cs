33e16aacb1e852e8807b1ac64d210a6352e71533
timreimherr <timreimherr@gmail.com>
2017-02-07 20:14:45 -0600

    created basic methods for Category entities

--- Blog/GamingGuruBlog.Web/GamingGuruBlog.Domain/CategoryServices.cs
+++ Blog/GamingGuruBlog.Web/GamingGuruBlog.Domain/CategoryServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GamingGuruBlog.Domain.Interfaces;
+using GamingGuruBlog.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,48 @@ using System.Threading.Tasks;
 
 namespace GamingGuruBlog.Domain
 {
-    class CategoryServices
+    public class CategoryServices
     {
+        private ICategoryRepository _categoryRepo;
+        private IBlogCategoryRepository _blogCategoryRepo;
+
+        public CategoryServices(ICategoryRepository newCategoryRepo, IBlogCategoryRepository newBlogCategoryRepo)
+        {
+            _categoryRepo = newCategoryRepo;
+            _blogCategoryRepo = newBlogCategoryRepo;
+        }
+
+        public void AddCategory(Category newCategory)
+        {
+            if (newCategory != null)
+            {
+                _categoryRepo.AddCategory(newCategory);
+            }
+        }
+
+        public void DeleteCategory(int categoryID)
+        {
+            _categoryRepo.DeleteCategory(categoryID);
+        }
+
+        public void EditCategory(Category changedCategory)
+        {
+            _categoryRepo.EditCategory(changedCategory);
+        }
+
+        public Category GetCategory(int categoryID)
+        {
+            return _categoryRepo.GetCategory(categoryID);
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            return _categoryRepo.GetAllCategories();
+        }
+
+        public List<Category> GetUsedCategories()
+        {
+            return _categoryRepo.GetOnlyUsedCategories();
+        }
     }
 }
