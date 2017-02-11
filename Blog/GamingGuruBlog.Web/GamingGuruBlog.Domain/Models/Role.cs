d37540940d7ca403dd3f9b114340044702fd073e
timreimherr <timreimherr@gmail.com>
2017-02-10 19:33:02 -0600

    created Role model

---
+++ Blog/GamingGuruBlog.Web/GamingGuruBlog.Domain/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingGuruBlog.Domain.Models
+{
+    class Role
+    {
+        public int RoleId { get; set; }
+        public string Name { get; set; }
+    }
+}
