bd0072558c20e6b861138252575168979ceceda6
timreimherr <timreimherr@gmail.com>
2017-02-11 10:41:00 -0600

    added methods to TagServices class and created it's interface

--- Blog/GamingGuruBlog.Web/GamingGuruBlog.Domain/TagServices.cs
+++ Blog/GamingGuruBlog.Web/GamingGuruBlog.Domain/TagServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GamingGuruBlog.Domain.Interfaces;
+using GamingGuruBlog.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace GamingGuruBlog.Domain
 {
-    class TagServices
+    public class TagServices
     {
+        private ITagRepository _tagRepo;
+        private IBlogTagRepository _blogTagRepo;
+
+        public TagServices(ITagRepository newTagRepo, IBlogTagRepository newBlogTagRepo)
+        {
+            _tagRepo = newTagRepo;
+            _blogTagRepo = newBlogTagRepo;
+        }
+
+        public List<Tag> GetAllTags()
+        {
+            return _tagRepo.GetAssignedTags();
+        }
+
+        public List<Tag> AddCreatedTags(List<Tag> tagNames)
+        {
+            List<string> justTagNames = new List<string>();
+
+            foreach (var tag in tagNames)
+            {
+                justTagNames.Add(tag.TagName);
+            }
+            return _tagRepo.AddAllTags(justTagNames);
+        }
+
+        public void AddTagsToBlog(int blogID, List<Tag> tagIDs)
+        {
+            foreach (var tag in tagIDs)
+            {
+                _blogTagRepo.AddTagToBlog(blogID, tag.TagId);
+
+            }
+        }
+
     }
 }
