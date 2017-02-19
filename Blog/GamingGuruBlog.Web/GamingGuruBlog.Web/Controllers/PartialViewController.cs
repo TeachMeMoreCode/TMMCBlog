da89a94f9c82f22c52ec91c26cec7ffb7fc6d437
timreimherr <timreimherr@gmail.com>
2017-02-19 16:39:39 -0600

    added changes from other branch

--- Blog/GamingGuruBlog.Web/GamingGuruBlog.Web/Controllers/PartialViewController.cs
+++ Blog/GamingGuruBlog.Web/GamingGuruBlog.Web/Controllers/PartialViewController.cs
@@ -10,12 +10,18 @@ namespace GamingGuruBlog.Web.Controllers
     {
         private IBlogServices _blogServices;
         private IStaticPageServices _staticPageServices;
+        private ICategoryServices _categoryServices;
+        private ITagServices _tagServices;
+        private IUserServices _userServices;
 
 
-        public PartialViewController(IStaticPageServices newStaticPageServices, IBlogServices newBlogservices)
+        public PartialViewController(IBlogServices newBlogservices, IStaticPageServices newStaticPageServices, ICategoryServices newCategoryServices, ITagServices newTagServices, IUserServices newUserServices)
         {
             _blogServices = newBlogservices;
             _staticPageServices = newStaticPageServices;
+            _categoryServices = newCategoryServices;
+            _tagServices = newTagServices;
+            _userServices = newUserServices;
         }
         // GET: PartialView
         public PartialViewResult Action()
@@ -28,15 +34,15 @@ namespace GamingGuruBlog.Web.Controllers
         {
             AllBlogPostsVM result = new AllBlogPostsVM();
             result.AllBlogPosts = _blogServices.GetAllBlogPosts();
-            result.AllCategories = _blogServices.GetUsedCategories();
-            result.AllTags = _blogServices.GetAllTags();
+            result.AllCategories = _categoryServices.GetUsedCategories();
+            result.AllTags = _tagServices.GetAllTags();
 
             return PartialView("~/Views/Shared/_BlogWidgetPartial.cshtml", result);
         }
 
         public PartialViewResult AdminPanelTabbedUsers()
         {
-            var model = _blogServices.GetAllUsers();
+            var model = _userServices.GetAllUsers();
             return PartialView("~/Views/PartialView/AdminPanelTabbedUsers.cshtml", model);
         }
 
